34920a20d8633c43394256e5e8dbf1584910c06f
Eric Newport <kethinov@gmail.com>
2019-07-15 11:41:47 -0400

    1.0.1
    Various refactoring, add GitHub link, add Renovate, alter authors, update dependencies.

--- build.js
+++ build.js
@@ -13,7 +13,7 @@ const data = {
   name: chalk.red('              CACI International Inc'),
   work: chalk.blue('An information solutions and services company\n              in support of national security missions and\n              government transformation for Intelligence,\n              Defense, and Federal Civilian customers.'),
   twitter: chalk.gray('https://twitter.com/') + chalk.cyan('CACIIntl'),
-  // github: chalk.gray('https://github.com/') + chalk.cyan('CACI-International'),
+  github: chalk.gray('https://github.com/') + chalk.cyan('CACI-International'),
   linkedin: chalk.gray('https://linkedin.com/company/') + chalk.cyan('caci-international-inc'),
   web: chalk.cyan('https://caci.com'),
   hiring: chalk.red('              We\'re hiring!\n              Find your innovation and imagination at https://careers.caci.com'),
@@ -21,7 +21,7 @@ const data = {
   labelWork: chalk.gray.bold('      About:'),
   labelTwitter: chalk.gray.bold('    Twitter:'),
   labelnpm: chalk.gray.bold('        npm:'),
-  // labelGitHub: chalk.gray.bold('     GitHub:'),
+  labelGitHub: chalk.gray.bold('     GitHub:'),
   labelLinkedIn: chalk.gray.bold('   LinkedIn:'),
   labelWeb: chalk.gray.bold('        Web:'),
   labelCard: chalk.white('       Card:')
@@ -31,7 +31,7 @@ const newline = '\n'
 const heading = `${data.name}`
 const working = `${data.labelWork}  ${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
-// const githubing = `${data.labelGitHub}  ${data.github}`
+const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
 const webing = `${data.labelWeb}  ${data.web}`
 const hiring = `${data.hiring}`
@@ -41,7 +41,7 @@ const output = heading +
                newline + newline +
                working + newline + newline +
                twittering + newline +
-               // githubing + newline +
+               githubing + newline +
                linkedining + newline +
                webing + newline + newline +
                hiring + newline + newline +
